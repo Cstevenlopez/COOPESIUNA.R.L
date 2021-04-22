148863764b4e37b83f647c6dbb32a10ac18033b3
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-04-22 16:21:53 -0600

    Cambios en la plantilla wellcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,11 @@
 @extends('layouts.plantillabase')
 
 @section('content')
-  <div class="row">
-    <div class="col">
-      primer contenido
-    </div>
-    <div class="col">
-    segundo contenido
-    </div>
-    <div class="col">
-    tercer contenido
-    </div>
-  </div>
+<section id="imagen">
+<div class="container">
+
+</div>
+</section>
 
 @endsection
 
