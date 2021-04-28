d2faf03163b39a742586ab4a4942e2dd293b7b3c
TonyNavas <tonynavas777@gmail.com>
2021-04-28 15:49:54 -0600

    Agregue el modelo de la tabla de asistencias tecnicas

---
+++ database/migrations/2021_04_28_214049_create_vitacora_asistencias_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVitacoraAsistenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vitacora_asistencias', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('productor_id')->constrained('productors');
+            $table->string('actividad',50);
+            $table->string('objetivo',50);
+            $table->string('temas_abordados',100);
+            $table->string('dificultades',100);
+            $table->string('soluciones',100);
+            $table->string('recomendaciones',100);
+            $table->date('fecha');
+            $table->date('siguiente_visita');
+            $table->foreignId('usuario_id')->constrained('usuarios');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vitacora_asistencias');
+    }
+}
