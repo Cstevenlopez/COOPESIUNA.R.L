241d67f27291a11d804804f120a54ee8c540ea0d
TonyNavas <tonynavas777@gmail.com>
2021-04-28 15:32:38 -0600

    Cree el modelo de la tabla de productores

---
+++ database/migrations/2021_04_28_212636_create_productors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('finca_id')->constrained('finca');
+            $table->string('id_productor')->Unique();
+            $table->string('nombres',50);
+            $table->string('apellidos',50);
+            $table->string('numero_cedula',20);
+            $table->string('numero_telefono',50);
+            $table->string('foto');
+            $table->string('comunidad',50);
+            $table->string('municipio',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productors');
+    }
+}
