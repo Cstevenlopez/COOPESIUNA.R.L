e229c3991300a0e85cc1c944a7ccdc3792eb9c1c
TonyNavas <tonynavas777@gmail.com>
2021-04-28 14:54:10 -0600

    Agregue el modelo de la tabla de las fincas

---
+++ database/migrations/2021_04_28_204307_create_fincas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFincasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fincas', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_finca')->unique();
+            $table->string('nombre',50);
+            $table->string('legalidad',50);
+            $table->string('comunidad',50);
+            $table->string('municipio',50);
+            $table->string('departamento',50);
+            $table->string('pais',50);
+            $table->string('latitud',50);
+            $table->string('longitud',50);
+            $table->string('altitud',50);
+            $table->string('disponibilidad_energia',50);
+            $table->string('disponibilidad_agua',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fincas');
+    }
+}
