75a2cd492d16bce3fd5ce33bcf76589586f96c66
TonyNavas <tonynavas777@gmail.com>
2021-05-05 11:36:37 -0600

    Cambio en el formulario de registro

--- resources/views/usuarios/create.blade.php
+++ resources/views/usuarios/create.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.plantillabaseu');
+@extends('layouts.plantillabaseu')
 
 @section('contenido')
+<h3>Agregar usuarios</h3>
 <Form action="/usuarios" method="POST">
 @csrf()
 <div class="mb-3">
@@ -9,23 +10,23 @@
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Nombre</label>
-        <input type="text" id="name" name="name" step="any" class="form-control" tabindex="4">
+        <input type="text" id="name" name="name" class="form-control" tabindex="4">
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Usuario</label>
-        <input type="text" id="usuario" name="usuario" step="any" class="form-control" tabindex="4">
+        <input type="text" id="usuario" name="usuario" class="form-control" tabindex="4">
     </div>
     <div class="mb-3">
         <label for="" class="form-label">E mail</label>
-        <input type="text" id="email" name="email" step="any" class="form-control" tabindex="4">
+        <input type="text" id="email" name="email" class="form-control" tabindex="4">
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Contraseña</label>
-        <input type="text" id="password" name="password" step="any" class="form-control" tabindex="4">
+        <input type="text" id="password" name="password" class="form-control" tabindex="4">
     </div>
     <div class="mb-3">
         <label for="" class="form-label">Perfil</label>
-        <input type="text" id="perfil" name="perfil" step="any" class="form-control" tabindex="4">
+        <input type="text" id="perfil" name="perfil" class="form-control" tabindex="4">
     </div>
     <a href="/articulos" class="btn btn-secondary" tabindex="5">Cancelar</a>
     <button type="submit" class="btn btn-primary" tabindex="4">Guardar</button>
