174b98f9b4a816d602cc7c3e425521093837d232
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-05-12 14:31:57 -0600

    Agrege imagenes que utilizaremos

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Coopesiuna.R.L</b>',
+    'logo' => '<b>Admin</b>LTE',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Coopesiuna',
+    'logo_img_alt' => 'AdminLTE',
 
     /*
     |--------------------------------------------------------------------------
