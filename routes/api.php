5c7624abfc6541d894fe75fccc734addb95f679a
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-06-06 23:55:05 -0600

    rutas para la autenticacion en sanctum

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 use App\Http\Controllers\API\FincaController;
+use App\Http\Controllers\API\UserController;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +24,7 @@ Route::post('registro',[UsuarioController::class,'registro']);
 
 Route::post('acceso',[UsuarioController::class,'acceso']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('fincas',[FincaController::class,'index']);
@@ -28,6 +33,31 @@ Route::put('fincas/{finca}',[FincaController::class,'update']);
 Route::get('fincas/{finca}',[FincaController::class,'show']);
 Route::delete('fincas/{finca}',[FincaController::class,'destroy']);
 
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    $token = $user->createToken($request->device_name)->plainTextToken;
+
+    $response = [
+            'user' => $user,
+            'token' => $token,
+    ];
+
+    return response($response, 201);
+});
 /*
 Route::get('/fincas', 'FincaController@index');
 
